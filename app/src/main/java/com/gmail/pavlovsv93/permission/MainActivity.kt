4036c804f71319372c779c552af0c07890b189e5
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-02-08 12:00:17 +0300

    Permission READ_CONTACTS

--- app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.gmail.pavlovsv93.permission
 
-import android.content.Context
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.gmail.pavlovsv93.permission.databinding.ActivityMainBinding
+
+const val REQUEST_CODE = 42 // Код результата ответа, ожидаемый при получении подтверждения (задается разработчиком)
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,79 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnContacts.setOnClickListener {
+            checkPermission()
+        }
     }
+
+    private fun checkPermission() {
+        // работает от контекста
+        applicationContext?.let {
+            when{
+                // Разрешение дано
+                ContextCompat.checkSelfPermission(it, android.Manifest.permission.READ_CONTACTS) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                    //Выполняется если разрешение на допуск ЕСТЬ
+                    getContacts()
+                }
+                // указать поеснение перед подтверждением
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) ->{ // Вызов пояснения, для чего нужен доступ
+                    AlertDialog.Builder(this@MainActivity).setTitle("Доступ к контактам")                  // Задать заголовок окна пояснения
+                        .setMessage("Объяснение необходимости доступа")                     // Задать сообщение окна пояснения
+                        .setPositiveButton("Предоставить доступ") { _,_ ->              // Задать кнопку и текст кнопки подтверждения
+                            request()                                                // перезапросить на подтверждение доступа
+                        }
+                        .setNegativeButton("НЕТ"){ dialog, _ ->                         // Задать кнопку и тект кнопки отказа
+                            dialog.dismiss()                                                    // Скрыть окно запроса разрешения по нажатию кнопки
+                        }
+                        .create()                                                           //
+                        .show()                                                             // Показать диалоговое окно
+                }
+                else -> request()        // Запросить разрешение
+            }
+        }
+    }
+
+    private fun request(){
+        //arrayOf помещаются все запросы на допуск необходимые для работы приложения, в данной ситуации 1 из 1
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+
+        //как пример на два запроса
+        // requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS,  Manifest.permission.CAMERA), REQUEST_CODE)
+    }
+
+    //Переопределить метод onRequestPermissionsResult
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            REQUEST_CODE -> {
+                //Проверка полученого разрешения
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getContacts()
+                } else {
+                    // Пояснение пользователю, почему экран остается пустым или дальнейшее действие не доступно
+                    applicationContext?.let {
+                        // Вслучае вызова AlertDialog с activity необходимо передать контекст активити,
+                        // в ином случае it (контекст, от которого запущен AleartDialog)
+                        AlertDialog.Builder(this@MainActivity)
+                            .setTitle("Заголовок")
+                            .setMessage("Сообщение")
+                            .setNegativeButton("Закрыть"){ dialog, _ ->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getContacts() {
+        // обработка при достижении согласия
+    }
+
+
 }
