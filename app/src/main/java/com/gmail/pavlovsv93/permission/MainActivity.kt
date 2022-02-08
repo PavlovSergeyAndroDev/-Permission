37eb4be96dbc2628333a978009c3d9b4802170eb
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-02-08 09:16:42 +0300

    Add binding in project and init

--- app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.gmail.pavlovsv93.permission
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.gmail.pavlovsv93.permission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //подключение viewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
