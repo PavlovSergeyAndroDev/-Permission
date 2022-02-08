422cf05aaf3c81dae388ff42fb300ff52bdd0bcd
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-02-08 12:43:44 +0300

    Add method getContacts

--- app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/permission/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.gmail.pavlovsv93.permission
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.Context
 import android.content.pm.PackageManager
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import com.gmail.pavlovsv93.permission.databinding.ActivityMainBinding
 
-const val REQUEST_CODE = 42 // Код результата ответа, ожидаемый при получении подтверждения (задается разработчиком)
+const val REQUEST_CODE =
+    42 // Код результата ответа, ожидаемый при получении подтверждения (задается разработчиком)
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     private fun checkPermission() {
         // работает от контекста
         applicationContext?.let {
-            when{
+            when {
                 // Разрешение дано
                 ContextCompat.checkSelfPermission(it, android.Manifest.permission.READ_CONTACTS) ==
                         PackageManager.PERMISSION_GRANTED -> {
@@ -38,13 +45,14 @@ class MainActivity : AppCompatActivity() {
                     getContacts()
                 }
                 // указать поеснение перед подтверждением
-                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) ->{ // Вызов пояснения, для чего нужен доступ
-                    AlertDialog.Builder(this@MainActivity).setTitle("Доступ к контактам")                  // Задать заголовок окна пояснения
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> { // Вызов пояснения, для чего нужен доступ
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle("Доступ к контактам")                  // Задать заголовок окна пояснения
                         .setMessage("Объяснение необходимости доступа")                     // Задать сообщение окна пояснения
-                        .setPositiveButton("Предоставить доступ") { _,_ ->              // Задать кнопку и текст кнопки подтверждения
+                        .setPositiveButton("Предоставить доступ") { _, _ ->              // Задать кнопку и текст кнопки подтверждения
                             request()                                                // перезапросить на подтверждение доступа
                         }
-                        .setNegativeButton("НЕТ"){ dialog, _ ->                         // Задать кнопку и тект кнопки отказа
+                        .setNegativeButton("НЕТ") { dialog, _ ->                         // Задать кнопку и тект кнопки отказа
                             dialog.dismiss()                                                    // Скрыть окно запроса разрешения по нажатию кнопки
                         }
                         .create()                                                           //
@@ -55,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun request(){
+    private fun request() {
         //arrayOf помещаются все запросы на допуск необходимые для работы приложения, в данной ситуации 1 из 1
         requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
 
@@ -64,13 +72,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Переопределить метод onRequestPermissionsResult
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when(requestCode){
+        when (requestCode) {
             REQUEST_CODE -> {
                 //Проверка полученого разрешения
-                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContacts()
                 } else {
                     // Пояснение пользователю, почему экран остается пустым или дальнейшее действие не доступно
@@ -80,7 +92,7 @@ class MainActivity : AppCompatActivity() {
                         AlertDialog.Builder(this@MainActivity)
                             .setTitle("Заголовок")
                             .setMessage("Сообщение")
-                            .setNegativeButton("Закрыть"){ dialog, _ ->
+                            .setNegativeButton("Закрыть") { dialog, _ ->
                                 dialog.dismiss()
                             }
                             .create()
@@ -91,9 +103,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // обработка при достижении согласия
+    @SuppressLint("Range")
     private fun getContacts() {
-        // обработка при достижении согласия
+
+        applicationContext?.let {
+            // Получаем ContactResolver у контакта
+            val contactResolver: ContentResolver = it.contentResolver
+
+            //Отправляем запрос на получение контактов и получаем ответ  в виде Cursor
+            val cursorWithContacts: Cursor? = contactResolver
+                .query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null,
+                    ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+                )
+
+            cursorWithContacts?.let { cursor ->
+                for(i in 0..cursor.count) {
+                    if (cursor.moveToPosition(i)) {
+                        val name = cursor.getString(
+                            cursor.getColumnIndex(
+                                ContactsContract.Contacts.DISPLAY_NAME
+                            )
+                        )
+                        addView(it, name)
+                    }
+                }
+
+            }
+        }
+
     }
 
-
+    private fun addView(context: Context, name: String?) {
+        binding.containerContacts.addView(AppCompatTextView(context).apply {
+            text = name
+            textSize = resources.getDimension(R.dimen.padding)
+        })
+    }
 }
